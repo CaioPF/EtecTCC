a56545b0c5837fbc83a6b0024a57966798ed4039
Caio <caio.palma.fernandes@gmail.com>
2023-08-17 20:50:05 -0300

    Front: Cabecalho, rodape, menu e imagem de produto. Php: pré-banco. Banco: por fazer.

---
+++ php/imgProdutos.php
@@ -0,0 +1,31 @@
+<!--imagnes e preço dos produtos-->
+
+<div class = "container-fluid">
+  <div class = "row">
+
+    <div class = "col-sm-3">
+      <img src = "https://placehold.it/450x320" class = "img-responsive" style = "width:100%">
+      <div><h1>Nome do produto</h1></div>
+      <div><h4>R$500,00</h4></div>
+    </div>
+
+    <div class = "col-sm-3">
+      <img src = "https://placehold.it/450x320" class = "img-responsive" style = "width:100%">  
+      <div><h1>Nome do produto</h1></div>
+      <div><h4>R$500,00</h4></div>
+    </div>
+
+    <div class = "col-sm-3">
+      <img src = "https://placehold.it/450x320" class = "img-responsive" style = "width:100%">
+      <div><h1>Nome do produto</h1></div>
+      <div><h4>R$500,00</h4></div>
+    </div>
+
+    <div class = "col-sm-3">
+      <img src = "https://placehold.it/450x320" class = "img-responsive" style = "width:100%">
+      <div><h1>Nome do produto</h1></div>
+      <div><h4>R$500,00</h4></div>
+    </div>
+
+  </div>
+</div>
