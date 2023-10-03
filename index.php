4481ed084afcfc22eadb26aa1ec28723beb2cb06
felipesampaii <felipe.galdino.sampaio@gmail.com>
2023-10-03 13:46:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,30 +7,47 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Protector Kings</title>
 
+<link rel="stylesheet" href="index.css">
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-<!-------------------------------------------------------------------------------------------->
-  <style type = "text/css">
-    .navbar{ margin-bottom: 0; } /*STYLE --> tira a borda entre o menu e o texto */
-  </style>
-<!-------------------------------------------------------------------------------------------->
+<?php
+include 'conexao.php'; 
+include 'links.php';
+?>
 </head>
 
 <body>
-  
 <?php  
-include 'conexao.php';
-include 'menu.php'; /*menu de navegação do site*/
+include 'menu.php';
 include 'carrossel.html';
-include 'imgProdutos.php'; /*imagnes e preço dos produtos*/
-include 'rodaPE.html';
-?>
 
+$consulta = $mysqli->query('select imagem_produto, nome_produto, preco_produto, quantidade_produto, pasta_imagem from produto');
+?>
+<br><br>
+
+<div class = "container-fluid">
+  <div class = "row">
+  <?php   while($exibe = $consulta->fetch_assoc()){  ?>
+    <div class = "col-sm-3">
+    <div class = "bvvvxW">
+    </br>
+      <img src="foto_produto/<?php echo $exibe['pasta_imagem']; ?>/<?php echo trim($exibe['imagem_produto']); ?>.jpg" class="img-responsive imgm" style=""> <!--TRIM remove todos os possiveis espaços que podem atrapalhar o código-->
+
+       <div><h4 style="text-align: center;"><b><?php echo mb_strimwidth($exibe['nome_produto'], 0, 15, '...'); ?></b></h4></div> <!--mb_strimwidth limita o tanto de caracteres que é visivel-->
+
+      <div><h5 style="text-align: center;">R$ <?php echo number_format($exibe['preco_produto'], 2, ',','.'); ?></h5></div> <!--number_format faz com que o preço fique no formato padrão BR-->
+      
+      <div class="text-center">
+        <button class="btn btn-three">
+          <span class="glyphicon glyphicon-info-sign" style="Color: cadetblue"> Detalhes</span>
+        </button>
+      </div>
+      <?php include 'botao_esgotado.php'; ?></br>
+      </div>
+    </div>
+    <?php } ?>
+  </div>
+</div>
+
+<?php include 'rodaPE.html'; ?>
 </body> 
 </html> 
