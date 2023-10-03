8f2ab2a65f29df708d627f4b384797ecf878a532
felipesampaii <felipe.galdino.sampaio@gmail.com>
2023-10-03 13:21:57 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Protector Kings</title>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<!-------------------------------------------------------------------------------------------->
+  <style type = "text/css">
+    .navbar{ margin-bottom: 0; } /*STYLE --> tira a borda entre o menu e o texto */
+  </style>
+<!-------------------------------------------------------------------------------------------->
+</head>
+
+<body>
+  
+<?php  
+include 'conexao.php';
+include 'menu.php'; /*menu de navegação do site*/
+include 'carrossel.html';
+include 'imgProdutos.php'; /*imagnes e preço dos produtos*/
+include 'rodaPE.html';
+?>
+
+</body> 
+</html> 
