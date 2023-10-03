042752d636c02813c066f8b99194434e1bf981f8
felipesampaii <felipe.galdino.sampaio@gmail.com>
2023-10-03 13:47:38 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -5,32 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.main.js"></script>
+
+    <?php include 'links.php'; ?>
+    
 </head>
 
 <body>
 <?php  
 include 'conexao.php';
 ?>
+
+<!------------------------------------------------------------------------------------------------->
+<!--Menu-->
 <div class="nav">
     <nav class="navbar navbar-inverse"><!--Apague o inverse para estar alterando a cor do menu de navegação-->
     <div class="container-fluid"><!--Essa linha cria uma borda entre o menu e o texto-->
         
         <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>              <!--Esse é o botão hamburguer que aparece quando o site-->
+            <span class="icon-bar"></span>                              <!-- nao está em tela cheia-->
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
         <a class="navbar-brand" href="index.php">Protector Kings</a>
         </div>
         
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="cadastro.php"><span> Cadastro</a></li>
-            <li><a href="login.php"><span> Login</a></li>
+            <?php  include 'session_start.php';?>
+            
         </ul>
         </div>
     </div>
     </nav>
 </div>
+<!------------------------------------------------------------------------------------------------->
+
 <h1>Estamos em manutenção na página de contato</h1>
 
 <?php include 'rodape.html' ?>
